a6c67cedcda0995d9ed0a2a29ba585d40d255d45
Prasham <theprasham@gmail.com>
2024-10-19 15:42:02 -0700

    Update SplineTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/SplineTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/SplineTest.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode.tuning;
 
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -15,15 +17,25 @@ public final class SplineTest extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         Pose2d beginPose = new Pose2d(0, 0, 0);
 
-        PinpointDrive drive = new PinpointDrive(hardwareMap,beginPose);
+        PinpointDrive drive = new PinpointDrive(hardwareMap, beginPose);
+        //MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
 
-            waitForStart();
-
-            Actions.runBlocking(
-                drive.actionBuilder(beginPose)
+        TrajectoryActionBuilder tra = drive.actionBuilder(beginPose)
                         .splineTo(new Vector2d(30, 30), Math.PI / 2)
-                        .splineTo(new Vector2d(0, 60), Math.PI)
-                        .build());
+                        .splineTo(new Vector2d(0, 60), Math.PI);
 
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()){
+            Actions.runBlocking(
+                    new SequentialAction(
+                            tra.build()
+                    )
+            );
+
+        }
     }
 }
