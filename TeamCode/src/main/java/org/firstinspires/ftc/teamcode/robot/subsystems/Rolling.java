951196f29c5108be1dba5b9e8d50415130aa7939
TheDudeWhoCodes <bhagat.aaroh@gmail.com>
2024-10-26 17:31:21 -0700

    Modify Intake Positions (Now correct!)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/Rolling.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/Rolling.java
@@ -23,14 +23,13 @@ public class Rolling implements Subsystem {
         flipRight.setPosition(position);
     }
     // Preset Intake rotations
-    // The position values below are test values i.e. modify these to be the actual position values.
     public void setIntakeUp(){
-        flipRight.setPosition(1);
-        flipLeft.setPosition(1);
+        flipRight.setPosition(0.8);
+        flipLeft.setPosition(0.8);
     }
     public void setIntakeDown(){
-        flipLeft.setPosition(0);
-        flipRight.setPosition(0);
+        flipLeft.setPosition(0.2);
+        flipRight.setPosition(0.2);
     }
     @Override
     public void update() {
