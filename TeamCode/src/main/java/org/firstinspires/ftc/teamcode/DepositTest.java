235847e885fac9ae2994bbb3cfc0ec3ffc8dd064
Arya Cherukuri <67491274+AryaCherukuri@users.noreply.github.com>
2024-10-27 15:46:45 -0700

    Update DepositTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DepositTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DepositTest.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;    
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServoImplEx;
 import com.qualcomm.robotcore.hardware.DcMotor;
