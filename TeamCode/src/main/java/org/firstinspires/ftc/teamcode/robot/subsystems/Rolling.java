bfab9771cdca512d0b0da5e7f40bb8630660819d
TheDudeWhoCodes <bhagat.aaroh@gmail.com>
2024-10-26 17:26:50 -0700

    Add Intake Positions (Not yet complete)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/Rolling.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/Rolling.java
@@ -17,8 +17,20 @@ public class Rolling implements Subsystem {
     public void setIntakePower(double power) {
         intakeMotor.setPower(power);
     }
-    public void intakeFlip(){
-
+    //Dynamic Intake rotation
+    public void setIntakePosition(double position){
+        flipLeft.setPosition(position);
+        flipRight.setPosition(position);
+    }
+    // Preset Intake rotations
+    // The position values below are test values i.e. modify these to be the actual position values.
+    public void setIntakeUp(){
+        flipRight.setPosition(1);
+        flipLeft.setPosition(1);
+    }
+    public void setIntakeDown(){
+        flipLeft.setPosition(0);
+        flipRight.setPosition(0);
     }
     @Override
     public void update() {
