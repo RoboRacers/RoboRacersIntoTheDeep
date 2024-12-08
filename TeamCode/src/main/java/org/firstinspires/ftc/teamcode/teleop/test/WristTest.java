12698b15f0b856f7f9c0cb125e07648d87c28429
Prasham <theprasham@gmail.com>
2024-12-08 07:51:42 -0800

    Create WristTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/test/WristTest.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.teleop.test;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "Wrist Test", group = "16481-IntoTheDeep")
+public class WristTest extends LinearOpMode {
+
+    Servo rotateClaw;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        rotateClaw = hardwareMap.get(Servo.class, "rotateClaw");
+
+        while (!isStopRequested()){
+            rotateClaw.setPosition(0);
+        }
+    }
+}
